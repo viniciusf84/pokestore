7c28139adef21eb3368f9decebfaf6ef1d2969aa
inicius <viniciusf84@hotmail.com>
2021-06-16 01:26:31 -0300

    Create Loading Content component

---
+++ src/components/LoadingContent/LoadingContent.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { LoadingContentStyled } from './LoadingContent.styled';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faSpinner } from '@fortawesome/free-solid-svg-icons';
+
+const LoadingContent = ({ isLoading, loadingText, children }) => {
+  if (isLoading) {
+    return (
+      <LoadingContentStyled>
+        <div className="loading-content__wrapper">
+          <p>{loadingText}</p>
+          <FontAwesomeIcon icon={faSpinner} size="3x" spin />
+        </div>
+      </LoadingContentStyled>
+    )
+  }
+
+  return children;
+}
+
+export default LoadingContent;
---
+++ src/components/LoadingContent/LoadingContent.styled.js
@@ -0,0 +1,13 @@
+import styled from 'styled-components';
+import { centerContent } from '../../styles/variables';
+
+export const LoadingContentStyled = styled.div`  
+  ${centerContent()}; 
+	margin-bottom: 60px;
+
+	.loading-content__wrapper {    
+		p {
+			margin-bottom: 16px;
+		}
+	}
+`;
