cdcbbd4867d1a359853bd637312081b6832a231c
inicius <viniciusf84@hotmail.com>
2021-06-16 01:28:10 -0300

    Create Home component

---
+++ src/containers/Home/Home.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { HomeStyled } from './Home.styled';
+
+// Containers
+import Results from '../Results';
+
+function Home() {
+	return (
+		<HomeStyled id="home">
+			<main className="main-content">
+        <Results />
+			</main>
+		</HomeStyled>
+	);
+}
+
+export default Home;
---
+++ src/containers/Home/Home.styled.js
@@ -0,0 +1,16 @@
+import styled from 'styled-components';
+import { rem } from 'polished';
+
+export const HomeStyled = styled.section`
+	.home__icon {
+		margin-bottom: 30px;
+	}
+
+	p {
+		font-size: ${rem('13px')};
+	}
+
+	.btn {
+		margin-top: 50px;
+	}
+`;
