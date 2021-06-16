1f711a71fc4f5c8924e71b42b3ce2b84484a7ba2
inicius <viniciusf84@hotmail.com>
2021-06-16 01:28:54 -0300

    Create Search component

---
+++ src/containers/Search/Search.js
@@ -0,0 +1,34 @@
+import React, { useContext, useCallback } from 'react';
+import { debounce } from 'lodash';
+import { SearchContext } from '../../contexts/SearchContext';
+
+// components
+import SearchInput from '../../components/SearchInput';
+
+export default function Search() {
+	const searchContext = useContext(SearchContext);
+	const {
+		setSearchString,
+		setUpdateList,
+    setMessage,
+	} = searchContext.actions;
+
+	const onHandleChange = useCallback(
+		debounce((value) => {
+			if (value.length > 3) {
+				setUpdateList(true);
+				setSearchString(value);
+				setMessage('');
+			}
+		}, 600),
+		[],
+	);
+
+	return (							
+    <SearchInput
+      name="search-input"
+      placeholder="Search for your favorite pokemon"
+      onChange={(e) => onHandleChange(e.target.value)}
+    />	   
+	);
+}
