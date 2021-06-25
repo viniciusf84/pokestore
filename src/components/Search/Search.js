9e20d08f5ba3f4b014f8040f08c5c1d644592898
inicius <viniciusf84@hotmail.com>
2021-06-25 15:38:30 -0300

    Handle change function fix

--- src/components/Search/Search.js
+++ src/components/Search/Search.js
@@ -1,32 +1,27 @@
 import React, { useContext, useCallback } from 'react';
 import { debounce } from 'lodash';
-import { SearchContext } from '../../contexts/SearchContext';
+import { ShopContext } from '../../contexts/ShopContext';
 
 // components
 import SearchInput from '../../components/SearchInput';
 
 export default function Search() {
-	const searchContext = useContext(SearchContext);
-	const {
-		setSearchString,
-    setMessage,
-	} = searchContext.actions;
+	const shopContext = useContext(ShopContext);
+	const { setSearchString, setMessage } = shopContext.actions;
 
 	const onHandleChange = useCallback(
 		debounce((value) => {
-			if (value.length > 3) {
-				setSearchString(value);
-				setMessage('');
-			}
+			setSearchString(value);
+			setMessage('');
 		}, 600),
 		[],
 	);
 
-	return (							
-    <SearchInput
-      name="search-input"
-      placeholder="Search for your favorite pokemon"
-      onChange={(e) => onHandleChange(e.target.value)}
-    />	   
+	return (
+		<SearchInput
+			name="search-input"
+			placeholder="Search for your favorite pokemon"
+			onChange={(e) => onHandleChange(e.target.value)}
+		/>
 	);
 }
