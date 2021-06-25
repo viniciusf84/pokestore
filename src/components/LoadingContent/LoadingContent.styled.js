ac1f734052360b96f22dbb9ad6f3c4f7ebdff8bd
inicius <viniciusf84@hotmail.com>
2021-06-25 15:34:38 -0300

    Loading content style fix

--- src/components/LoadingContent/LoadingContent.styled.js
+++ src/components/LoadingContent/LoadingContent.styled.js
@@ -1,11 +1,12 @@
 import styled from 'styled-components';
 import { centerContent } from '../../styles/variables';
 
-export const LoadingContentStyled = styled.div`  
-  ${centerContent()}; 
+export const LoadingContentStyled = styled.div`
+	${centerContent()};
+	padding: 30px 0;
 	margin-bottom: 60px;
 
-	.loading-content__wrapper {    
+	.loading-content__wrapper {
 		p {
 			margin-bottom: 16px;
 		}
