aafcfc17686fb8baf23f6c3160b9bd6b1547ebfa
inicius <viniciusf84@hotmail.com>
2021-06-26 19:10:05 -0300

    Create checkout modal

---
+++ src/containers/CheckoutModal/CheckoutModal.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+// component
+import Modal from '../../components/Modal';
+
+export default function CheckoutModal() {
+	return (
+		<Modal>
+			<p>Thank you so much! </p>
+		</Modal>
+	);
+}
