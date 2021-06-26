1351d28129156c7e23b59e8d30f495a98535fd52
inicius <viniciusf84@hotmail.com>
2021-06-26 15:13:34 -0300

    Create UseLocalState function

---
+++ src/utils/UseLocalState.js
@@ -0,0 +1,21 @@
+import { useState, useEffect } from 'react';
+
+export default function useLocalState(key, initial) {
+	const [value, setValue] = useState(() => {
+		if (typeof window !== 'undefined') {
+			const saved = window.localStorage.getItem(key);
+
+			if (saved !== null) {
+				return JSON.parse(saved);
+			}
+		}
+
+		return initial;
+	});
+
+	useEffect(() => {
+		window.localStorage.setItem(key, JSON.stringify(value));
+	}, [value]);
+
+	return [value, setValue];
+}
