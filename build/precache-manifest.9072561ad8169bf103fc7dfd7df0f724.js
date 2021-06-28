ed45ce68128bf7d287bc8d321d76a16131307d7f
inicius <viniciusf84@hotmail.com>
2021-06-28 02:02:03 -0300

    Add build docs

---
+++ build/precache-manifest.9072561ad8169bf103fc7dfd7df0f724.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "12cc27e6746855b684c1",
+    "url": "./static/js/main.1e67782f.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  },
+  {
+    "revision": "56ba7fdb89c761af0b6b",
+    "url": "./static/js/2.4a12118a.chunk.js"
+  },
+  {
+    "revision": "1099d4d93f822f4f1184c83eb3466adc",
+    "url": "./index.html"
+  }
+];
