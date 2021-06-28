3d59735a936d57ded7a7be987925bf42cf54743a
inicius <viniciusf84@hotmail.com>
2021-06-27 21:30:36 -0300

    Isolate GetImageUrl function

---
+++ src/utils/GetImageUrl.js
@@ -0,0 +1,9 @@
+export default function GetImageUrl(pokemon) {
+	const imageUrl =
+		pokemon &&
+		pokemon.sprites &&
+		(pokemon.sprites.other['official-artwork'].front_default ||
+			pokemon.sprites.front_default);
+
+	return imageUrl;
+}
