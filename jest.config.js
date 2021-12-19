a24a41e38519a0920287c36783c53cb5bf26f935
inicius <viniciusf84@hotmail.com>
2021-12-19 11:36:44 -0300

    Install react testing library

---
+++ jest.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+	testIgnorePatterns: ['/node-modules'],
+	transform: {
+		'^.*\\(js|jsx)$': '<rootDir>/node-modules/babel-jest',
+	},
+	setupFilesAfterEnv: ['<rootDir>/setupTests.js'],
+	testEnvironment: 'jsdom',
+};
