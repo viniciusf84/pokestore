fbc7f27242195b51f5b6991a4aa726922663620a
inicius <viniciusf84@hotmail.com>
2021-12-19 11:37:00 -0300

    Apply footer tests

---
+++ src/components/Footer/Footer.spec.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import { toBeInTheDocument } from '@testing-library/jest-dom';
+
+import Footer from './Footer';
+
+const textProp = 'Pokestore 2021';
+
+describe('Footer', () => {
+	it('renders given text', () => {
+		render(<Footer text={textProp} />);
+
+		expect(screen.getByText(textProp)).toBeInTheDocument();
+	});
+});
